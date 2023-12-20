c7227fdff5bbd80c8759dde5f3f74832d0a8ac8e
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-12-20 12:57:29 +0530

    add more cards

--- src/components/CardsBlog.tsx
+++ src/components/CardsBlog.tsx
@@ -3,16 +3,16 @@ import { CardsData } from "../common/Helper";
 const CardsBlog = () => {
   return (
     <>
-      <div className="container xl:max-w-[1100px] pt-12 lg:pt-20 px-4 sm:px-8 xl:px-0 mx-auto">
-        <div className="lg:flex justify-between">
+      <div className="container xl:max-w-[1100px] pt-12 lg:pt-5 px-4 sm:px-8 xl:px-0 mx-auto">
+        <div className="lg:flex justify-between flex-wrap">
           {CardsData.map((obj, index) => {
             return (
-              <a
-                href="/blog-post"
-                key={index}
-                className="lg:w-[47%] pt-12 lg:pt-0"
-              >
-                <img src={obj.img} alt="card-img-1" />
+              <a href="/blog-post" key={index} className="lg:w-[47%]">
+                <img
+                  src={obj.img}
+                  alt="card-img-1"
+                  className="pt-12 lg:pt-16"
+                />
                 <div className="flex justify-between flex-col">
                   <div>
                     <button className="text-sm font-Roboto text-white_off_3 font-medium px-[17px] pt-[3px] pb-1 bg-light_blue rounded-[3px] mt-5">
