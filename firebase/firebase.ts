fea25ba6071afcccb02445e29b2ab26e6501ba94
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-12-21 10:02:01 +0530

    add env and firebase.ts file

---
+++ firebase/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.VITE_APP_API_KEY,
+  authDomain: process.env.VITE_APP_AUTH_DOMAIN,
+  projectId: process.env.VITE_APP_PROJECT_ID,
+  storageBucket: process.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
